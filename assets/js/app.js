b6f1f13474dcc782887b30e5f8942742e6530693
Marcos Andre <marcosdev.me@gmail.com>
2021-12-21 09:36:30 -0300

    docs: organizing code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 const get = (element) => document.querySelector(element);
 const getAll = (element) => document.querySelectorAll(element);
 
+// Initial Data
 let currentColor = 'black';
+let screen = get('#tela');
 
+// Events
 getAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', handleColorClick);
 });
 
+// Functions
 function handleColorClick(e) {
     const color = e.target.getAttribute('data-color');
     currentColor = color;
